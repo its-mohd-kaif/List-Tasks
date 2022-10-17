3648b97f71e27cc06e0d318a35c2b5876f1247ad
its-mohd-kaif <mohdkaif@cedcoss.com>
2022-10-17 13:35:56 +0530

    changes done of task3

--- list/src/Task3/Ques3.js
+++ list/src/Task3/Ques3.js
@@ -1,58 +1,57 @@
 import React, { useState, useEffect } from "react";
 import "./style3.css";
 function Ques3() {
- 
   // Usestate for holding list value
-  const [group1, setGroup1] = useState();
-  const [group2, setGroup2] = useState();
-  // UseEffect for rendering one time list when page is loaded
-  useEffect(() => {
-    const li1 = ["Ajax", "jQuery", "javaScirpt", "MooTools", "Prototype", "Dojo"];
-    const li2 = ["ASP.NET", "C#", "VB.NET", "Java", "PHP", "Python"];
-    setGroup1(li1);
-    setGroup2(li2);
-  }, []);
+  const [group1, setGroup1] = useState([
+    "Ajax",
+    "jQuery",
+    "javaScirpt",
+    "MooTools",
+    "Prototype",
+    "Dojo",
+  ]);
+  const [group2, setGroup2] = useState([
+    "ASP.NET",
+    "C#",
+    "VB.NET",
+    "Java",
+    "PHP",
+    "Python",
+  ]);
   // Transfer data from list1 to list2
   const leftToright = () => {
-    // console.log("LI1",li1)
-    // console.log("LI2",li2)
     let selected = document.querySelectorAll("#group1 option:checked");
     let values = Array.from(selected).map((el) => el.value);
 
-    console.log("First",values)
+    console.log("First", values);
 
     values.forEach((element) => {
-      li1.forEach((element2, index) => {
+      group1.forEach((element2, index) => {
         if (element === element2) {
-          li1.splice(index, 1);
+          group1.splice(index, 1);
         }
       });
-      li2.push(element);
+      group2.push(element);
     });
-  
-    console.log("LI1",li1)
-    console.log("LI2",li2)
-    setGroup1(li1);
-    setGroup2(li2);
-   
+    setGroup1([...group1]);
+    setGroup2([...group2]);
   };
   // Transfer data from list2 to list1
   const rightToleft = () => {
     let selected = document.querySelectorAll("#group2 option:checked");
     let values = Array.from(selected).map((el) => el.value);
-    console.log("Second",values)
-
+    console.log("Second", values);
 
     values.forEach((element) => {
-      li2.forEach((element2, index) => {
+      group2.forEach((element2, index) => {
         if (element === element2) {
-          li2.splice(index, 1);
+          group2.splice(index, 1);
         }
       });
-      li1.push(element);
+      group1.push(element);
     });
-    setGroup1(li1);
-    setGroup2(li2);
+    setGroup1([...group1]);
+    setGroup2([...group2]);
   };
 
   return (
